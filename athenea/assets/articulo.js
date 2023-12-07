bcbd3b20c502bdb09c70533eca4348c0b2d3fbce
Juan <juanmunoz.juan@gmail.com>
2023-12-07 12:10:17 +0100

    Add files via upload

--- athenea/assets/articulo.js
+++ athenea/assets/articulo.js
@@ -1,208 +1,139 @@
-/////////////////////
-/// popup imagen ////
-/////////////////////
+// Pone texto de divs sidebar en modal:
+const exampleModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+  keyboard: false
+});
+const modalTitle = document.querySelector('#exampleModal .modal-title');
+const modalContent = document.getElementById('modal-content');
 
-document.addEventListener('DOMContentLoaded', function() {
-    const imagenes = document.querySelectorAll('.imagen');
-    const popup = document.getElementById('popup-imagen');
-    const popupImage = document.getElementById('popup-image');
-    const popupNumero = document.getElementById('popup-numero');
-    const popupTitulo = document.getElementById('popup-titulo');
-    const closeButton = document.getElementById('close-button');
+document.addEventListener('click', (event) => {
+  const link = event.target.closest('[data-bs-toggle="modal"]');
+  if (link) {
+    const contentId = link.getAttribute('data-bs-content');
+    const contentDiv = document.querySelector(contentId);
 
-    imagenes.forEach(function(imagen) {
-        imagen.addEventListener('click', function() {
-            const imgUrl = imagen.getAttribute('src');
-            const numeroFigura = this.parentElement.querySelector('.numero-figura').textContent;
-            const tituloFigura = this.parentElement.querySelector('.titulo-figura').textContent;
+    if (contentDiv) {
+      // Busca cualquier elemento hijo del enlace que tenga el atributo data-bs-title
+      const iconElement = Array.from(link.children).find(child => child.getAttribute('data-bs-title'));
 
-            popupImage.src = imgUrl;
-            popupNumero.textContent = numeroFigura;
-            popupTitulo.textContent = tituloFigura;
+      // Obtiene el título del tooltip del icono
+      const tooltipTitle = iconElement ? iconElement.getAttribute('data-bs-title') : '';
 
-            popup.style.display = 'flex';
+      // Asigna el título del tooltip como data-bs-title al enlace
+      link.setAttribute('data-bs-title', tooltipTitle);
+
+      // Utiliza el título del tooltip como título del modal
+      modalTitle.textContent = tooltipTitle;
+      modalContent.innerHTML = contentDiv.innerHTML;
+      exampleModal.show();
+
+      // Agrega un listener para el clic dentro del contenido del modal
+      modalContent.addEventListener('click', closeModalOnClick);
+    }
+  }
+});
+
+// Función para cerrar el modal si se hace clic en un enlace dentro del contenido del modal
+function closeModalOnClick(event) {
+  const linkInModal = event.target.closest('a');
+  if (linkInModal) {
+    exampleModal.hide();
+    modalContent.removeEventListener('click', closeModalOnClick); // Elimina el listener después de cerrar el modal
+  }
+}
+
+// FIN pone texto de divs sidebar en modal
+
+// Script para copiar dinámicamente el contenido de autores al modal
+
+    $('#modalAutores').on('show.bs.modal', function (event) {
+        var modal = $(this);
+        var autoresContent = $('.autores').html();
+        $('#modalBodyAutores').html(autoresContent);
+
+        
+    });
+
+// Pone texto de divs imágenes en modal
+document.addEventListener('DOMContentLoaded', function () {
+  const imageModal = new bootstrap.Modal(document.getElementById('ModalImage'), {
+    keyboard: false
+  });
+  const imageModalTitle = document.querySelector('#ModalImage .modal-title');
+  const imageModalContent = document.getElementById('modal-image-content');
+  const modalCloseButton = document.getElementById('modalCloseButton');
+
+  document.addEventListener('click', (event) => {
+    const container = event.target.closest('.image-container');
+    if (container) {
+      // imageModalTitle.textContent = container.getAttribute('data-bs-title');
+      
+      // Clona la imagen y aplica la clase 'modal-image'
+      const originalImage = container.querySelector('img');
+      const clonedImage = originalImage.cloneNode(true);
+      clonedImage.classList.add('modal-image');
+
+      // Crea un contenedor para la información y agrega la clase 'modal-info'
+      const infoContainer = document.createElement('div');
+      infoContainer.classList.add('modal-info');
+
+      // Agrega la información de la clase 'numero-figura'
+      const numeroFigura = container.querySelector('.numero-figura');
+      if (numeroFigura) {
+        const numeroFiguraContainer = document.createElement('div');
+        numeroFiguraContainer.classList.add('modal-info-item');
+        numeroFiguraContainer.textContent = numeroFigura.textContent;
+        infoContainer.appendChild(numeroFiguraContainer);
+      }
+
+      // Agrega la información de la clase 'titulo-figura'
+      const tituloFigura = container.querySelector('.titulo-figura');
+      if (tituloFigura) {
+        const tituloFiguraContainer = document.createElement('div');
+        tituloFiguraContainer.classList.add('modal-info-item');
+        tituloFiguraContainer.textContent = tituloFigura.textContent;
+        infoContainer.appendChild(tituloFiguraContainer);
+      }
+
+      // Agrega el contenedor de información junto con la imagen al contenido del modal
+      imageModalContent.innerHTML = '';
+      imageModalContent.appendChild(clonedImage);
+      imageModalContent.appendChild(infoContainer);
+
+      imageModal.show();
+    }
+  });
+});
+
+// FIN pone texto de divs imágenes en modal
+
+ 
+// Popover referencias ORIGINAL
+    document.addEventListener('DOMContentLoaded', function () {
+    // Inicializa los popovers de Bootstrap
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl, {
+        html: true,
+        content: function () {
+          var referenceTarget = popoverTriggerEl.getAttribute('data-reference-id');
+          var detailsContent = document.querySelector(referenceTarget).innerHTML;
+          var referenceId = referenceTarget.substring(1); // Elimina el # del inicio
+
+          // Construye el contenido del popover con una línea en blanco y el enlace "Ir a la referencia"
+          return detailsContent + '<a href="#' + referenceId + '" class="close-popover">Ir a la referencia</a>';
+        },
+      });
+    });
+
+    // Cierra el popover al hacer clic en cualquier lugar de la página
+    document.addEventListener('click', function (event) {
+      if (!event.target.closest('.popover') || event.target.classList.contains('close-popover')) {
+        popoverList.forEach(function (popover) {
+          popover.hide();
         });
+      }
     });
+  });
+// FIN popovers referencias
 
-    closeButton.addEventListener('click', function() {
-        popup.style.display = 'none';
-    });
-});
-// Fin popup imagen //
-
-///////////////////////////////////////////////////////
-/// Funciones para popup referencias bibliográficas ///
-///////////////////////////////////////////////////////
-const referencePopup = document.getElementById('reference-popup');
-let activeReferenceId = null;
-
-document.querySelectorAll('.reference').forEach(function(reference) {
-    reference.addEventListener('click', function(event) {
-        event.stopPropagation();
-        const referenceId = this.getAttribute('data-reference-id');
-
-        if (referenceId === activeReferenceId) {
-            // Si se hace clic en el mismo enlace que abrió el popup, ciérralo
-            referencePopup.style.display = 'none';
-            activeReferenceId = null;
-        } else {
-            showReferencePopup(referenceId, this); // Pasa el enlace como argumento
-        }
-    });
-});
-
-function showReferencePopup(referenceId, referenceElement) {
-    const referenceRect = referenceElement.getBoundingClientRect();
-    const mainContent = document.querySelector('.main-content');
-    const mainContentRect = mainContent.getBoundingClientRect();
-    const popupWidth = referencePopup.clientWidth;
-
-    let popupLeft = referenceRect.left - mainContentRect.left + window.scrollX;
-    let popupTop = referenceRect.bottom + window.scrollY;
-
-    // Verificar si hay desbordamiento a la derecha si el popup se coloca alineado a la izquierda del enlace
-    if (popupLeft + popupWidth > mainContentRect.right) {
-        popupLeft = mainContentRect.right - popupWidth;
-    }
-
-    // Ajustar la posición hacia la izquierda lo mínimo necesario para evitar el desbordamiento
-    if (popupLeft < mainContentRect.left) {
-        popupLeft = mainContentRect.left;
-    }
-
-    const popupReferenceDetails = document.getElementById('popup-reference-details');
-    popupReferenceDetails.innerHTML = getReferenceData(referenceId);
-
-    referencePopup.style.top = popupTop + 'px';
-    referencePopup.style.left = popupLeft + 'px';
-
-    referencePopup.style.display = 'block';
-
-    activeReferenceId = referenceId;
-
-    const goToReferenceList = document.getElementById('go-to-reference-list');
-    goToReferenceList.setAttribute('href', `#${referenceId}`);
-    goToReferenceList.onclick = function() {
-        referencePopup.style.display = 'none';
-        activeReferenceId = null;
-    };
-
-    const closeIcon = document.getElementById('close-icon');
-    closeIcon.addEventListener('click', function() {
-        referencePopup.style.display = 'none';
-        activeReferenceId = null;
-    });
-}
-
-function getReferenceData(referenceId) {
-    const referenceElement = document.getElementById(referenceId);
-    return referenceElement.innerHTML;
-}
-
-document.body.addEventListener('click', function(event) {
-    if (event.target !== referencePopup && !referencePopup.contains(event.target)) {
-        referencePopup.style.display = 'none';
-        activeReferenceId = null;
-    }
-});
-
-//////////////////////////////////////////
-/// Funciones para desplegable resumen ///
-//////////////////////////////////////////
-
-// Función para mostrar/ocultar el resumen con efecto persiana
-const toggleResumen = document.getElementById('toggle-resumen');
-const resumen = document.getElementById('resumen');
-const keywords = document.getElementById('keywords')
-
-let isResumenVisible = false;
-
-toggleResumen.addEventListener('click', function() {
-    if (isResumenVisible) {
-        resumen.style.maxHeight = '0';
-        isResumenVisible = false;
-    } else {
-        resumen.style.maxHeight = resumen.scrollHeight + 'px';
-        isResumenVisible = true;
-    }
-});
-
-//////////////////////////////////////////
-/// Funciones para información autores ///
-//////////////////////////////////////////
-
-// Función para mostrar el popup al hacer clic en el enlace del autor
-var autorLinks = document.querySelectorAll('.autorLink');
-var popup = document.querySelector('.popup');
-var activeAutorLink = null; // Variable para llevar un registro del enlace activo
-
-for (var i = 0; i < autorLinks.length; i++) {
-    autorLinks[i].addEventListener('click', function(e) {
-        e.preventDefault();
-
-        if (this === activeAutorLink && popup.style.display === 'block') {
-            // Si se hace clic en el mismo enlace que abrió el popup, ciérralo
-            popup.style.display = 'none';
-            activeAutorLink = null;
-        } else {
-            var nombreAutor = this.getAttribute('data-nombre');
-            var universidad = this.getAttribute('data-universidad');
-            var correo = this.getAttribute('data-correo');
-            var orcid = this.getAttribute('data-orcid');
-            var variable = this.getAttribute('data-variable');
-            var otro = this.getAttribute('data-otro');
-            var nombreAutorElement = popup.querySelector('.nombreAutor');
-            var universidadElement = popup.querySelector('.universidad');
-            var correoElement = popup.querySelector('.correo');
-            var orcidElement = popup.querySelector('.orcid');
-            var variableElement = popup.querySelector('.variable');
-            var otroElement = popup.querySelector('.otro');
-
-            nombreAutorElement.textContent = nombreAutor;
-            if (universidad) {
-                universidadElement.innerHTML = `<i class="fas fa-university"></i> ${universidad || ''}`;
-            } else {
-                universidadElement.innerHTML = '';
-            }
-            if (correo) {
-                correoElement.innerHTML = `<i class="fas fa-envelope"></i>  ${correo}`;
-            } else {
-                correoElement.innerHTML = '';
-            }
-            if (orcid) {
-                orcidElement.innerHTML = `<i class="fab fa-orcid"></i>  ${orcid || ''}`;
-            } else {
-                orcidElement.innerHTML = ' ';
-            }
-            if (variable) {
-                variableElement.innerHTML = variable;
-            } else {
-                variableElement.innerHTML = ' ';
-            }
-            if (otro) {
-                otroElement.textContent = otro;
-            } else {
-                otroElement.textContent = ' ';
-            }
-
-            // Obtener las coordenadas del autorLink
-            var rect = this.getBoundingClientRect();
-            var autorLinkTop = rect.top + window.scrollY;
-            var autorLinkLeft = rect.left + window.scrollX;
-
-            // Posicionar el popup debajo del autorLink
-            popup.style.top = autorLinkTop + this.offsetHeight + 'px';
-            popup.style.left = autorLinkLeft + 'px';
-
-            popup.style.display = 'block';
-            activeAutorLink = this; // Actualizar el enlace activo
-        }
-    });
-}
-
-window.addEventListener('click', function(e) {
-    if (e.target !== popup && !e.target.classList.contains('autorLink')) {
-        popup.style.display = 'none';
-        activeAutorLink = null; // Restablecer el enlace activo
-    }
-});
+ 
